fb2015a68b4556cc0247715e145df30efa9bd59d
JulioCGonzalez <jcgonzalez_95@hotmail.com>
2014-10-18 15:50:15 -0600

    Trabajo

---
+++ Clases/JavaApplication/src/javaapplication/Matriz.java
@@ -0,0 +1,15 @@
+package javaapplication;
+public class Matriz {
+public static void main(String[] args) {
+ Ejercicio01 oEjercicio01=new Ejercicio01();
+        oEjercicio01.cargar();
+        oEjercicio01.intercambiar();
+        oEjercicio01.imprimir();
+    
+//  Ejercicio02 oEjercicio02=new Ejercicio02();
+//        oEjercicio02.cargar();
+//        oEjercicio02.imprimirVertices();
+//               
+    }
+    
+}
